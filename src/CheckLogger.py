5031ed3d3cf585713e13cac38764f869b16d9dec
nero-luci <cornel.punga@gmail.com>
2015-05-31 16:53:59 +0300

    [Vectron|CheckGuard] Update license for main and CheckLogger. Update last revision

--- src/CheckLogger.py
+++ src/CheckLogger.py
@@ -1,3 +1,30 @@
+'''
+ *  Copyright (C) 2015 Touch Vectron
+ *
+ *  Author: Cornel Punga
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ *
+ *	Filename: CheckLogger.py
+ *	Loggind module
+ *
+ *
+ *	Last revision: 05/31/2015
+ *
+'''
+
 from logging import Logger, FileHandler, Formatter
 
 class CheckLogger(Logger):
