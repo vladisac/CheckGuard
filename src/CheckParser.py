fc90702c559099e7ca8b944bfcd27af427368b2a
nero-luci <cornel.punga@gmail.com>
2015-05-31 16:45:47 +0300

    [Vectron|CheckGuard] Add log messages. Changes to imports. Added backup list in case a regex failes.

--- src/CheckParser.py
+++ src/CheckParser.py
@@ -26,10 +26,12 @@
  *
 '''
 
-import codecs
-import re
+from datetime import datetime
+from codecs import open as copen
+from re import search, sub
+from inspect import stack
 from subprocess import Popen
-import datetime
+from CheckLogger import check_logger
 
 
 class CheckParser(object):
@@ -62,37 +64,42 @@ class CheckParser(object):
             self._filename = r"C:\Vectron\VPosPC\files.txt"
 
     def read_file(self):
-        with codecs.open(self.filename, "r", encoding="latin-1") as fh:
+        check_logger.debug("{0}: {1}".format(stack()[0][3], "_____START_____"))
+        with copen(self.filename, "r", encoding="latin-1") as fh:
             fh.seek(self.position)
             line = fh.readline()
             include = False
             while line:
-                if re.search("\*{2,}", line):
+                if search("\*{2,}", line):
                     if not include:
                         include = True
                         line = fh.readline()
-                        if re.search("\*{2,}", line):
+                        if search("\*{2,}", line):
                             include = False
                     else:
                         include = False
                 if include:
                     self.check_data.append(line)
                 if "= Cut =" in line and self.check_data != []:
+                    check_logger.debug("{0}: {1}".format(stack()[0][3], self.check_data))
                     self.generate_new_check()
                     self.check_data = []
                 line = fh.readline()
             self.position = fh.tell()
+            check_logger.debug("{0}: {1}".format(stack()[0][3], "_____END_____"))
 
     @staticmethod
     def write_2_file(to_print):
         header_line = "KARAT\n"
         footer_line = "T0000010000 TOTAL\nEND KARAT\n"
         file_bon = r"C:\Listener\bon.txt"
+        check_logger.debug("{0}: {1}".format(stack()[0][3], to_print))
         with open(file_bon, "w") as fp:
             fp.write(header_line)
             for item in to_print:
                 fp.write(item)
             fp.write(footer_line)
+        print("Tiparire -> Status: OK!")
 
     @staticmethod
     def execute_batch_file():
@@ -102,21 +109,27 @@ class CheckParser(object):
 
     def generate_new_check(self):
         check_to_print = []
-        time = datetime.datetime.now()
+        time = datetime.now()
         for elem in self.check_data:
-            reg_ex = re.search('\d+\,\d+', elem)
-            assert reg_ex, "Price regex failed"
-            price = elem[reg_ex.start():reg_ex.end()]
-            price = (re.sub(',', '', price)).rjust(8, '0')
+            check_logger.debug("{0}: {1}".format(stack()[0][3], elem))
+            backup_list = list(filter(lambda x: x != '',elem.split(' ')))
+            reg_ex = search('\d+\,\d+', elem)
+            if reg_ex:
+                price = elem[reg_ex.start():reg_ex.end()]
+            else:
+                price = backup_list[5]
+            price = (sub(',', '', price)).rjust(8, '0')
             decimals = "2"
-            reg_ex = re.search('\d+', elem)
-            assert reg_ex, "Quantity regex failed"
-            quantity = elem[reg_ex.start():reg_ex.end()]
-            quantity = (re.sub(',', '', quantity)).rjust(6, '0') + "000"
-            reg_ex = re.search('\d{1,2}%', elem)
+            reg_ex = search('\d+', elem)
+            if reg_ex:
+                quantity = elem[reg_ex.start():reg_ex.end()]
+            else:
+                quantity = backup_list[0]
+            quantity = (sub(',', '', quantity)).rjust(6, '0') + "000"
+            reg_ex = search('\d{1,2}%', elem)
             if reg_ex:
                 tva = elem[reg_ex.start():reg_ex.end()]
-                tva = re.sub('%', '', tva)
+                tva = sub('%', '', tva)
                 if tva == "24":
                     if 7 <= time.hour < 24:
                         tva = "1"
@@ -127,18 +140,18 @@ class CheckParser(object):
                 else:
                     tva = "4"
             else:
-                if 7 <= time.hour < 24:
-                        tva = "1"
-                else:
-                        tva = "2"
+                tva = backup_list[6]
             subgroup = "1"
             group = "1"
-            reg_ex = re.search('[a-zA-Z]{2,}[\S\s]?[a-zA-Z]*[\S\s]?[a-zA-Z]*', elem)
-            assert reg_ex, "Product name regex failed"
-            prod_name = elem[reg_ex.start():reg_ex.end()]
-            prod_name.strip(' ')
+            reg_ex = search('[a-zA-Z]{2,}[\S\s]?[a-zA-Z]*[\S\s]?[a-zA-Z]*', elem)
+            if reg_ex:
+                prod_name = elem[reg_ex.start():reg_ex.end()]
+                prod_name.strip(' ')
+            else:
+                prod_name = backup_list[3]
             final_check = '*' + prod_name + " " * (24 - len(prod_name)) + price + decimals + quantity + tva + subgroup \
                           + group + '\n'
+            check_logger.debug("{0}: {1}".format(stack()[0][3], final_check))
             check_to_print.append(final_check)
         CheckParser.write_2_file(check_to_print)
         CheckParser.execute_batch_file()
@@ -148,6 +161,7 @@ def read_init_pos():
     pos_filepath = r"C:\Vectron\pos.txt"
     with open(pos_filepath, "r") as fp:
         init_pos = fp.readline()
+        check_logger.debug("{0}: {1}".format(stack()[0][3], init_pos))
         if init_pos == '':
             init_pos = 0
     return int(init_pos)
@@ -161,6 +175,7 @@ def write_init_pos(pos):
             pos = "0"
 
     pos_filepath = r"C:\Vectron\pos.txt"
+    check_logger.debug("{0}: {1}".format(stack()[0][3], pos))
     with open(pos_filepath, "w") as fp:
         fp.write(pos)
 
@@ -169,4 +184,5 @@ def get_file_end_pos():
         filename = r"C:\Vectron\VPosPC\files.txt"
         with open(filename, "r") as ff:
             ff.seek(0, 2)
+            check_logger.debug("{0}: {1}".format(stack()[0][3], ff.tell()))
             return int(ff.tell())
